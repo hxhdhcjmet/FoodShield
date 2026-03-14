b1861aceb755991755ed43a0915e4b3f9c86a2fd
lyj <2871148635@qq.com>
2026-03-14 15:27:40 +0800

    实现merkle tree结构,实现merkle root生成功能

--- project/crypto/merkle.py
+++ project/crypto/merkle.py
@@ -20,15 +20,76 @@ def hash_message(order_id : str, sender_pid : str, content : str, timestamp : st
     :param timestamp : 消息产生的时间戳
     :return : 消息体的哈希值(64位十六进制字符串)
     """
-    message_str = f"{order_id}{sender_pid}{content}{timestamp}"
+    # message_str = f"{order_id}{sender_pid}{content}{timestamp}"
+    # '|'拼接分隔可以防止出现如 12+3 = 1+23= 123 的碰撞
+    message_str = "|".join([order_id,sender_pid,content,timestamp])
 
     return generate_hash(message_str)
+
+class MerkleTree:
+    """
+    基于迭代列表的轻量级、高性能Merkle Tree
+    """
+    def __init__(self,leaves : list):
+        # leaves为包含多条消息 msg_hash 的列表
+        self.leaves = leaves
+
+    def get_root(self)->str:
+        """
+        计算并返回Merkle Root
+        """
+        # 如果没有日志,返回固定空字符串的Hash
+        if not self.leaves:
+            return generate_hash("")
+        
+        current_level = self.leaves
+
+        # 当前层级的节点数大于1时,继续向上构建
+        while len(current_level) > 1:
+            next_level = []
+
+            # 步长为2,两两提取节点
+            for i in range(0,len(current_level),2):
+                left = current_level[i]
+                # 奇数个节点时,复制最后一个节点让它自己配对,防止落单
+                if i+1 < len(current_level):
+                    right = current_level[i+1]
+                else:
+                    right = left
+                # 拼接左右哈希值并进行二次哈希
+                combined = left + right
+                next_level.append(generate_hash(combined))
+
+            # 新的一层赋值给当前层,继续下一次while循环
+            current_level = next_level
+        return current_level[0]
 
 
 
 if __name__ == "__main__":
-    data = "hello world"
-    print(generate_hash(data))
+    print("="*30,"测试Merkle Tree","="*30)
+
+    h1 = generate_hash('msg1')
+    h2 = generate_hash('msg2')
+    h3 = generate_hash('msg3')
+    leaves_list = [h1,h2,h3]
+    tree = MerkleTree(leaves_list)
+
+    root = tree.get_root()
+
+    print(f"叶子节点数:{len(leaves_list)}")
+    print(f"生成的mekle root:{root}")
+
+    print("="*30,"测试篡改","="*30)
+    h4fake = generate_hash('msg4')
+    changed_leaves = [h1,h2,h3,h4fake]
+    fake_tree = MerkleTree(changed_leaves)
+    fake_root = fake_tree.get_root()
+    print(f"叶子节点数:{len(changed_leaves)}")
+    print(f"篡改后的merkle root:{fake_root}")
+    
+
+
 
 
 
